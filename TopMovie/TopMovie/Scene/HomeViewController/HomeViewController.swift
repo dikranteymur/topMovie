465436ffe452313c807bf722f26ce941f0479601
dikranteymur <mdteymur@gmail.com>
2023-12-21 19:35:38 +0300

    Added Home scene

---
+++ TopMovie/TopMovie/Scene/HomeViewController/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  TopMovie
+//
+//  Created by Dikran Teymur on 21.12.2023.
+//
+
+import UIKit
+import TinyConstraints
+
+final class HomeViewController: BaseViewController<HomeViewModel> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubviews()
+        configureContents()
+    }
+}
+
+// MARK: - UILayout
+extension HomeViewController {
+    
+    private func addSubviews() {
+        
+    }
+}
+
+// MARK: - ConfigureContents
+extension HomeViewController {
+    
+    private func configureContents() {
+        
+    }
+}
---
+++ TopMovie/TopMovie/Scene/HomeViewController/HomeViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewModel.swift
+//  TopMovie
+//
+//  Created by Dikran Teymur on 21.12.2023.
+//
+
+import Foundation
+
+protocol HomeViewModelProtocol {
+    
+}
+
+final class HomeViewModel: BaseViewModel {
+    
+}
