75677b03933f52f388114e5f38fd73187c488dd0
dikranteymur <mdteymur@gmail.com>
2023-12-22 22:48:05 +0300

    Started app service

---
+++ TopMovie/TopMovie/Network/AppService.swift
@@ -0,0 +1,16 @@
+//
+//  AppService.swift
+//  TopMovie
+//
+//  Created by Dikran Teymur on 22.12.2023.
+//
+
+import Foundation
+
+final class AppService: BaseService {
+    
+}
+
+extension AppService {
+    
+}
---
+++ TopMovie/TopMovie/Network/BaseService.swift
@@ -0,0 +1,17 @@
+//
+//  BaseService.swift
+//  TopMovie
+//
+//  Created by Dikran Teymur on 22.12.2023.
+//
+
+import Foundation
+import Alamofire
+
+protocol BaseServiceProtocol {
+    
+}
+
+class BaseService {
+    
+}
