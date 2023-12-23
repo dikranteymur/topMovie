8bc64861856237571c5c0cbd7b4df109384b73a5
dikranteymur <mdteymur@gmail.com>
2023-12-23 23:12:48 +0300

    Added UIFont extensions

---
+++ TopMovie/TopMovie/Extension/Extension+UIFont.swift
@@ -0,0 +1,60 @@
+//
+//  Extension+UIFont.swift
+//  TopMovie
+//
+//  Created by Dikran Teymur on 23.12.2023.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    private enum FontsName: String {
+        case thin = "Lato-Thin"
+        case light = "Lato-Light"
+        case regular = "Lato-Regular"
+        case bold = "Lato-Bold"
+    }
+    
+    // FontSize: 12
+    class var fontThin12: UIFont {
+        return UIFont(name: FontsName.thin.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 12)
+    }
+    class var fontLight12: UIFont {
+        return UIFont(name: FontsName.light.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 12)
+    }
+    class var fontRegular12: UIFont {
+        return UIFont(name: FontsName.regular.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 12)
+    }
+    class var fontBold12: UIFont {
+        return UIFont(name: FontsName.bold.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 12)
+    }
+    
+    // FontSize: 14
+    class var fontThin14: UIFont {
+        return UIFont(name: FontsName.thin.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 14)
+    }
+    class var fontLight14: UIFont {
+        return UIFont(name: FontsName.light.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 14)
+    }
+    class var fontRegular14: UIFont {
+        return UIFont(name: FontsName.regular.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 14)
+    }
+    class var fontBold14: UIFont {
+        return UIFont(name: FontsName.bold.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 14)
+    }
+    
+    // FontSize: 16
+    class var fontThin16: UIFont {
+        return UIFont(name: FontsName.thin.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 16)
+    }
+    class var fontLight16: UIFont {
+        return UIFont(name: FontsName.light.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 16)
+    }
+    class var fontRegular16: UIFont {
+        return UIFont(name: FontsName.regular.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 16)
+    }
+    class var fontBold16: UIFont {
+        return UIFont(name: FontsName.bold.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 16)
+    }
+}
