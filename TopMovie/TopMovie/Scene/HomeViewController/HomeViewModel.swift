07fe8c26e45e61c40c9baafc34de112e3e84f137
dikranteymur <mdteymur@gmail.com>
2023-12-23 20:58:33 +0300

    Add service into HomeViewModel

--- TopMovie/TopMovie/Scene/HomeViewController/HomeViewModel.swift
+++ TopMovie/TopMovie/Scene/HomeViewController/HomeViewModel.swift
@@ -8,9 +8,26 @@
 import Foundation
 
 protocol HomeViewModelProtocol {
-    
+    func loadShows()
+    func loadShowsWith(page: Int)
 }
 
-final class HomeViewModel: BaseViewModel {
+final class HomeViewModel: BaseViewModel, HomeViewModelProtocol {
     
+    func loadShows() {
+        let request = PopularShowsRequest(page: 1)
+        app.service.request(with: request) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let response):
+                print("Response: \(response)")
+            case .failure(let error):
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func loadShowsWith(page: Int) {
+        
+    }
 }
