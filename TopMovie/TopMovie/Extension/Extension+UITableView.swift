28a2005a9ed6b90b16a21c604bd6134dfcfd1742
dikranteymur <mdteymur@gmail.com>
2023-12-24 22:04:49 +0300

    Refactoring UITableView extension deque cell and added footer view

--- TopMovie/TopMovie/Extension/Extension+UITableView.swift
+++ TopMovie/TopMovie/Extension/Extension+UITableView.swift
@@ -13,8 +13,25 @@ extension UITableView {
         register(T.self, forCellReuseIdentifier: String(describing: T.self))
     }
     
-    func dequeueReusableCell<T: UITableViewCell>(cellClass: T.Type, indexPath: IndexPath) -> T? {
-        guard let cell = dequeueReusableCell(withIdentifier: String(describing: T.self), for: indexPath) as? T else { return nil }
+    func dequeueReusableCell<T: UITableViewCell>(indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: String(describing: T.self), for: indexPath) as? T else {
+            fatalError("Cell not found: \(String(describing: T.self))")
+        }
         return cell
+    }
+    
+    func showFooterView() {
+        let view = UIView(frame: .init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        let spinner = UIActivityIndicatorView(style: .medium)
+        spinner.style = .large
+        spinner.color = .colorOrange
+        view.addSubview(spinner)
+        spinner.centerInSuperview()
+        spinner.startAnimating()
+        tableFooterView = view
+    }
+    
+    func removeFooterView() {
+        tableFooterView = nil
     }
 }
