d28026d40cc7312d148f2fcc54864f2fa18639af
dikranteymur <mdteymur@gmail.com>
2023-12-24 22:03:54 +0300

    Refactoring UIFont extension font size

--- TopMovie/TopMovie/Extension/Extension+UIFont.swift
+++ TopMovie/TopMovie/Extension/Extension+UIFont.swift
@@ -32,29 +32,43 @@ extension UIFont {
     
     // FontSize: 14
     class var fontThin14: UIFont {
-        return UIFont(name: FontsName.thin.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 14)
+        return UIFont(name: FontsName.thin.rawValue, size: 14) ?? UIFont.systemFont(ofSize: 14)
     }
     class var fontLight14: UIFont {
-        return UIFont(name: FontsName.light.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 14)
+        return UIFont(name: FontsName.light.rawValue, size: 14) ?? UIFont.systemFont(ofSize: 14)
     }
     class var fontRegular14: UIFont {
-        return UIFont(name: FontsName.regular.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 14)
+        return UIFont(name: FontsName.regular.rawValue, size: 14) ?? UIFont.systemFont(ofSize: 14)
     }
     class var fontBold14: UIFont {
-        return UIFont(name: FontsName.bold.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 14)
+        return UIFont(name: FontsName.bold.rawValue, size: 14) ?? UIFont.systemFont(ofSize: 14)
     }
     
     // FontSize: 16
     class var fontThin16: UIFont {
-        return UIFont(name: FontsName.thin.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 16)
+        return UIFont(name: FontsName.thin.rawValue, size: 16) ?? UIFont.systemFont(ofSize: 16)
     }
     class var fontLight16: UIFont {
-        return UIFont(name: FontsName.light.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 16)
+        return UIFont(name: FontsName.light.rawValue, size: 16) ?? UIFont.systemFont(ofSize: 16)
     }
     class var fontRegular16: UIFont {
-        return UIFont(name: FontsName.regular.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 16)
+        return UIFont(name: FontsName.regular.rawValue, size: 16) ?? UIFont.systemFont(ofSize: 16)
     }
     class var fontBold16: UIFont {
-        return UIFont(name: FontsName.bold.rawValue, size: 12) ?? UIFont.systemFont(ofSize: 16)
+        return UIFont(name: FontsName.bold.rawValue, size: 16) ?? UIFont.systemFont(ofSize: 16)
+    }
+    
+    // FontSize: 32
+    class var fontThin32: UIFont {
+        return UIFont(name: FontsName.thin.rawValue, size: 32) ?? UIFont.systemFont(ofSize: 32)
+    }
+    class var fontLight32: UIFont {
+        return UIFont(name: FontsName.light.rawValue, size: 32) ?? UIFont.systemFont(ofSize: 32)
+    }
+    class var fontRegular32: UIFont {
+        return UIFont(name: FontsName.regular.rawValue, size: 32) ?? UIFont.systemFont(ofSize: 32)
+    }
+    class var fontBold32: UIFont {
+        return UIFont(name: FontsName.bold.rawValue, size: 32) ?? UIFont.systemFont(ofSize: 32)
     }
 }
